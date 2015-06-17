76b4cd41f2931ecf5b4a49e6b9f5230b14c15c50
puffsnow <brucechen2@gmail.com>
2015-06-17 16:19:57 +0800

    initialize view with family tree

--- app/controllers/family_tree_controller.rb
+++ app/controllers/family_tree_controller.rb
@@ -1,4 +1,7 @@
 class FamilyTreeController < ApplicationController
   def index
+    @all_members = Member.all
+    @maximum_grade = Member.maximum('grade')
+    @minimum_grade = Member.minimum('grade')
   end
 end
