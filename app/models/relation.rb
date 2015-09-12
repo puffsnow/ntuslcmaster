60cf715c682f2750111838a26a44f7e430d6b296
puffsnow <brucechen2@gmail.com>
2015-09-12 15:20:55 +0800

    remove incorrect foreign key in relation model

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -1,4 +1,4 @@
 class Relation < ActiveRecord::Base
-  belongs_to :master, :class_name => "Member", :foreign_key => "master_id"
-  belongs_to :apprentice, :class_name => "Member", :foreign_key => "apprentice_id"
+  belongs_to :master, :class_name => "Member"
+  belongs_to :apprentice, :class_name => "Member"
 end
