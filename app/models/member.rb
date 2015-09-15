a6fa9eea245302ab3dc986fcd58c328a90d533ec
puffsnow <brucechen2@gmail.com>
2015-09-15 15:33:10 +0800

    make relations work for Member, I'm not sure if dependent is needed so I remove it

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,7 @@
 class Member < ActiveRecord::Base
-  has_many :master_relations, :foreign_key => "apprentice_id", :class_name => "Relation", :dependent => :destroy
+  has_many :master_relations, :foreign_key => "apprentice_id", :class_name => "Relation"
   has_many :masters, :through => :master_relations
-  has_many :apprentice_relations, :foreign_key => "master_id", :class_name => "Relation", :dependent => :destroy
+  has_many :apprentice_relations, :foreign_key => "master_id", :class_name => "Relation"
   has_many :apprentices, :through => :apprentice_relations
 
   belongs_to  :user,  :class_name => 'User'
--- app/models/relation.rb
+++ app/models/relation.rb
@@ -1,4 +1,4 @@
 class Relation < ActiveRecord::Base
-  belongs_to :master, :class_name => "Member"
-  belongs_to :apprentice, :class_name => "Member"
+  belongs_to :master, :foreign_key => "master_id", :class_name => "Member"
+  belongs_to :apprentice, :foreign_key => "apprentice_id", :class_name => "Member"
 end
