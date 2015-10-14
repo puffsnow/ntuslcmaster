908abb437c763fb467f0c471d66370284f569f7a
puffsnow <brucechen2@gmail.com>
2015-10-14 22:18:48 +0800

    create follow_relations controller

---
+++ app/controllers/follow_relations_controller.rb
@@ -0,0 +1,3 @@
+class FollowRelationsController < ApplicationController
+  
+end
---
+++ test/controllers/follow_relations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FollowRelationsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
